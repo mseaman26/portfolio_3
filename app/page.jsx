116526a09573a938a53b52e25b6fc36eb683ca16
michael seaman <mseaman26@gmail.com>
2023-12-01 17:01:03 -0800

    initial next and tailwind setup

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Home = () =>{
+  return (
+    <div className='bg-black text-white'>
+      test
+    </div>
+  )
+}
+export default Home
