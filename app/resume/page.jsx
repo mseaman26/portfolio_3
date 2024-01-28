ab2582257c49187092f7b301ba328e7863d7c6cb
michael seaman <mseaman26@gmail.com>
2024-01-27 16:50:10 -0800

    trying to get pdf to work on mobile

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -5,14 +5,14 @@ export default function ResumePage(){
 
     return(
         <main className={styles.container}>
-            <h1 className='myh1'>If you'd like to download a pdf copy of my resume, click <span><a
-    href="/images/Resume.pdf"
-    alt="alt text"
-    target="_blank"
-    download={true}
-    rel="noopener noreferrer"
-  >Here</a></span></h1>
-            <iframe src="/images/Resume.pdf" style={{border: 'none', width: '100%', height: '100%'}}></iframe>
+            <h1 className={styles.header}>If you'd like to download a pdf copy of my resume, click <span><a
+                href="/images/Resume.pdf"
+                alt="alt text"
+                target="_blank"
+                download={true}
+                rel="noopener noreferrer"
+            >Here</a></span></h1>
+            <embed src="/images/Resume.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0" style={{border: 'none', width: '100%', height: '100%'}}></embed>
         </main>
     )
 }
