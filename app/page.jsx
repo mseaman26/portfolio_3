78c94728e63f06933cea7835b338050ce0979f38
michael seaman <mseaman26@gmail.com>
2024-01-30 14:05:52 -0800

    resized headshot

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,26 @@ export default function Home() {
       <Dash/>
       <h2>Full-Stack Developer</h2>
       <div className={styles.headShot}>
-        <Image src={'/images/headshot.webp'} width={100} height={100} layout='responsive' alt='headshot of Michael Seaman'/>
+        <Image  
+          loading='lazy'
+          width={100} 
+          height={100} 
+          decoding="async"
+          layout='responsive' 
+          sizes="100vw"
+          alt='headshot of Michael Seaman'
+          data-nimg="1"
+          srcset="
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=640&q=75 640w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=750&q=75 750w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=828&q=75 828w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=1080&q=75 1080w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=1200&q=75 1200w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=1920&q=75 1920w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=2048&q=75 2048w,
+          /_next/image?url=%2Fimages%2Fheadshot.webp&w=3840&q=75 3840w
+        "
+        src="/_next/image?url=%2Fimages%2Fheadshot.webp&w=3840&q=75"/>
       </div>
       <SkillsSection/>
     </main>
