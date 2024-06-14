e08bcf55491ff002de4bd777f463c893c1eb2cd4
michael seaman <mseaman26@gmail.com>
2024-06-14 14:42:38 -0700

    commented out contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,14 +1,14 @@
 
-import React from "react";
-import styles from './contact.module.css'
-import ContactForm from "../Components/contact/contactForm";
+// import React from "react";
+// import styles from './contact.module.css'
+// import ContactForm from "../Components/contact/contactForm";
 
-const Contact = () => {
-    return(
-        <main className={styles.container}>
-            <h1>I love talking about ANYTHING coding related.  Feel free to hit me up! </h1>
-            <ContactForm/>
-        </main>
-    )
-}
-export default Contact;
+// const Contact = () => {
+//     return(
+//         <main className={styles.container}>
+//             <h1>I love talking about ANYTHING coding related.  Feel free to hit me up! </h1>
+//             <ContactForm/>
+//         </main>
+//     )
+// }
+// export default Contact;
